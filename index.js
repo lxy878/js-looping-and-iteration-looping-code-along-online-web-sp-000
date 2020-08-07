23138a7ff6ea5855e7f2268fbd0ca53e9cd6f383
Xiaoyi Liu <lxy878@gmail.com>
2020-08-07 17:44:57 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(array, word){
+  const messages = [];
+  for(let name of array){
+      messages.push(`Thank you, ${name}, for the wonderful ${word} gift!`);
+  }
+  return messages;
+}
+
+function countDown(number){
+  while(number>-1){
+    console.log(number);
+    number --;
+  }
+}
