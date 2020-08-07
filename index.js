f9b511c4a6d87ea9b7412ffbbd9d52be483fdd31
Xiaoyi Liu <lxy878@gmail.com>
2020-08-07 17:33:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solutions in this file
 function writeCards(array, word){
-  let messages = []
+  const messages = []
   for(i=0; i<array.length, i++){
       messages[i] = `Thank you, ${array[i]}, for the wonderful ${word} gift!`;
   }
