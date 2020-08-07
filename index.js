8c9abf114c7599a80d51e148ac1cbbbe3ba2e5e1
Xiaoyi Liu <lxy878@gmail.com>
2020-08-07 17:28:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solutions in this file
 function writeCards(array, word){
   for(let name of array){
-      console.log(`Thank you, ${name}, for the wonderful ${word} gift!`)
+      console.log(`Thank you, ${name}, for the wonderful ${word} gift!`);
   }
 }
